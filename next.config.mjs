f2a4c3c3c66852b826030b536d4625e401176637
Vishal Chavan <vishalmc.18@gmail.com>
2025-05-28 17:58:02 +0530

    next file changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
